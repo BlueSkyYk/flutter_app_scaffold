0fa564a0055af2a4f2222de71e04e568d8f36049
yinkai <kai.yin@fanyou-online.com>
2026-05-22 10:31:44 +0800

    0.2.0版本

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
   AppBootstrap.run(
     config: const AppConfig(
       env: AppEnv.dev,
-      apiBaseUrl: 'http://10.42.0.135:8085/dev/api',
+      apiBaseUrl: 'http://127.0.0.1:8085/dev/api',
     ),
     app: () => const ProviderScope(child: ExampleApp()),
   );
